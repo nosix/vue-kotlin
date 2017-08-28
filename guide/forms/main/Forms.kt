5c17961321851d94a761ae2e722694b64b83d71f
nosix <nosix@users.noreply.github.com>
2017-08-28 21:44:02 +0900

    add forms.html

---
+++ guide/forms/main/Forms.kt
@@ -0,0 +1,33 @@
+import org.musyozoku.vuekt.Vue
+import org.musyozoku.vuekt.json
+import kotlin.js.Json
+
+external interface Example1Model : Json {
+    var checkedNames: Array<String>
+}
+
+val example1 = Vue {
+    el = "#example-1"
+    data = json<Example1Model> {
+        checkedNames = emptyArray()
+    }
+}
+
+class OptionItem(val text: String, val value: String)
+
+external interface Example2Model : Json {
+    var selected: String
+    var options: Array<OptionItem>
+}
+
+val example2 = Vue {
+    el = "#example-2"
+    data = json<Example2Model> {
+        selected = "A"
+        options = arrayOf(
+                OptionItem("One", "A"),
+                OptionItem("Two", "B"),
+                OptionItem("Three", "C")
+        )
+    }
+}
