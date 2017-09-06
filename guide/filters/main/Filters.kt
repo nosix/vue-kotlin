e181dc4f9857c0440d97fb25c09ca6b0e3cde39a
nosix <i.nosix@gmail.com>
2017-09-06 16:27:00 +0900

    add comments and examples

---
+++ guide/filters/main/Filters.kt
@@ -0,0 +1,44 @@
+import org.musyozoku.vuekt.*
+
+//  new Vue({
+//    // ...
+//    filters: {
+//      capitalize: function (value) {
+//        if (!value) return ''
+//        value = value.toString()
+//        return value.charAt(0).toUpperCase() + value.slice(1)
+//      }
+//    }
+//  })
+
+@JsModule("vue")
+@JsNonModule
+@JsName("Vue")
+external class ExampleVue(options: ComponentOptions<ExampleVue>) : Vue {
+    var message: String
+    var rawId: Int
+    var arg2: String
+}
+
+val vm = ExampleVue(ComponentOptions {
+    el = ElementConfig("#example")
+    data = ObjectOrFactory(json<ExampleVue> {
+        message = "hello"
+        rawId = 1
+        arg2 = ".txt"
+    })
+    filters = json {
+        this["capitalize"] = {
+            it: String ->
+            it.capitalize()
+        }
+        this["formatId"] = {
+            it: Int, digits: Int ->
+            it.asDynamic().toFixed(digits) // FIXME: JavaScript の関数を実行
+        }
+        this["filterA"] = {
+            it: String, arg1: String, arg2: String ->
+            "$arg1$it$arg2"
+        }
+    }
+})
