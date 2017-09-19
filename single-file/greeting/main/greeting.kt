473f7a9d590ec069bafce79d9c5c949076a020b3
nosix <nosix@users.noreply.github.com>
2017-09-20 01:05:47 +0900

    Add sample and comment

--- single-file/greeting/main/greeting.kt
+++ single-file/greeting/main/greeting.kt
@@ -3,8 +3,17 @@ import org.musyozoku.vuekt.*
 @JsModule("greeting-component.vue")
 external val GreetingComponent: Component = definedExternally
 
+@JsModule("vue")
+@JsName("Vue")
+external class AppVue(options: ComponentOptions<AppVue>) : Vue {
+    var message: String
+}
+
 val vm = Vue(ComponentOptions {
     el = ElementConfig("#app")
+    data = Data(json<AppVue> {
+        message = "Vue & Kotlin"
+    })
     components = json {
         this["greeting"] = ComponentConfig(GreetingComponent)
     }
